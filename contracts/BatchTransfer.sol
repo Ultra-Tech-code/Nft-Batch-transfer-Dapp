8ce775e5e491489bb812051aff33b9c7a142597d
Ultra-tech-code <hizick27@gmail.com>
2023-02-28 09:09:08 +0100

    changes to the contract

--- contracts/BatchTransfer.sol
+++ contracts/BatchTransfer.sol
@@ -14,27 +14,33 @@ contract BatchTransfer is Ownable {
 
     event TransferSuccessfull(address indexed _from, IERC721 indexed _collection, address indexed _to);
 
-
     /**
         * @notice allows batch transfer of multiple NFTs
         * @dev the function assumes the NFTs belongs to the same collection
         * @param _from the owner of the NFTs
         * @param _collection the address of the NFT smart contract
         * @param _tokenIds an array of tokenIds to be transferred 
+        * @param _totalId lenght of tokena to be transferred
      */
-    function bulkTransfer(address _from, IERC721 _collection, address _to, uint256[] memory _tokenIds, uint256 _limit) external {
-        require(_to != address(0));
+    function bulkTransfer(address _from, IERC721 _collection, address _to, uint256[] memory _tokenIds, uint256 _totalId) external {
+        require(_totalId > 0, "Limit must be greater than 0");
+        require(_to != address(0), "invalid to address");
         require(_from != address(0));
-        uint totalIds = _tokenIds.length;
-         require(_limit > 0, "Limit must be greater than 0");
-        require(totalIds == _limit, "Number of tokens to transfer exceeds limit");
-        require(_collection.getApproved(_tokenIds[i]) == address(this), "Contract not approved for transfer");
-        
-        for (uint256 i = 0; i < totalIds; i++) {
+
+        uint _IdsLength = _tokenIds.length;
+
+        for (uint256 i = 0; i < _IdsLength; i++) {
+            if(_collection.getApproved(_tokenIds[i]) != address(this)){
+                continue;
+            }
             _collection.safeTransferFrom(_from, _to, _tokenIds[i]);
         }
 
         emit TransferSuccessfull(_from, _collection, _to);
+    }
+
+    function getAlltokenID() external {
+        
     }
 
 
@@ -43,12 +49,4 @@ contract BatchTransfer is Ownable {
                 _collection.safeTransferFrom(address(this), _to, _tokenIds[i]);
             }
         }
-
-        
-
-        
-
-     
-
-
 }
